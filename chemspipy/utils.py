e789c7ebe55102aa12964cb7d5b3ccb676d0ea85
Matt Swain <m.swain@me.com>
2014-09-12 18:37:42 +0100

    Add memoized property decorator to utils

---
+++ chemspipy/utils.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+chemspipy.utils
+~~~~~~~~~~~~~~~
+
+Miscellaneous utility functions.
+
+:copyright: Copyright 2014 by Matt Swain.
+:license: MIT, see LICENSE file for more details.
+"""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+import functools
+
+
+def memoized_property(fget):
+    """Decorator to create memoized properties."""
+    attr_name = '_{}'.format(fget.__name__)
+
+    @functools.wraps(fget)
+    def fget_memoized(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fget(self))
+        return getattr(self, attr_name)
+    return property(fget_memoized)
